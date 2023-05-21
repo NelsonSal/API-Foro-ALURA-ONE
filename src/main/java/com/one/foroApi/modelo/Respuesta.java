401acdfcf1f5c3a3bf080142b79b01fb9d04fe5d
NelsonJ <njsocode@gmail.com>
2023-05-20 20:11:14 -0500

    Clases completas--->Listo para empezar Topico

---
+++ src/main/java/com/one/foroApi/modelo/Respuesta.java
@@ -0,0 +1,90 @@
+package com.one.foroApi.modelo;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+
+@Entity (name="Respuesta")
+@Table(name="respuestas")
+@EqualsAndHashCode(of="id")
+public class Respuesta {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String mensaje;
+	private Topico topico;
+	private LocalDateTime fechaCreacion = LocalDateTime.now();
+	private Usuario autor;
+	private Boolean solucion = false;
+	
+	public Respuesta() {
+	}
+
+	public Respuesta(Long id, String mensaje, Topico topico, LocalDateTime fechaCreacion, Usuario autor,
+			Boolean solucion) {
+		this.id = id;
+		this.mensaje = mensaje;
+		this.topico = topico;
+		this.fechaCreacion = fechaCreacion;
+		this.autor = autor;
+		this.solucion = solucion;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public Topico getTopico() {
+		return topico;
+	}
+
+	public void setTopico(Topico topico) {
+		this.topico = topico;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Usuario getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Usuario autor) {
+		this.autor = autor;
+	}
+
+	public Boolean getSolucion() {
+		return solucion;
+	}
+
+	public void setSolucion(Boolean solucion) {
+		this.solucion = solucion;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/one/foroApi/modelo/Topico.java
@@ -0,0 +1,112 @@
+package com.one.foroApi.modelo;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+
+@Entity (name="Topico")
+@Table(name="topicos")
+@EqualsAndHashCode(of="id")
+public class Topico {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String mensaje;
+	private LocalDateTime fechaCreacion = LocalDateTime.now();
+	private StatusTopico status = StatusTopico.NO_RESPONDIDO;
+	private Usuario autor;
+	private Curso curso;
+	private List<Respuesta> respuestas = new ArrayList<>();
+	
+	public Topico() {
+	}
+
+	public Topico(Long id, String titulo, String mensaje, LocalDateTime fechaCreacion, StatusTopico status,
+			Usuario autor, Curso curso, List<Respuesta> respuestas) {
+		this.id = id;
+		this.titulo = titulo;
+		this.mensaje = mensaje;
+		this.fechaCreacion = fechaCreacion;
+		this.status = status;
+		this.autor = autor;
+		this.curso = curso;
+		this.respuestas = respuestas;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public LocalDateTime getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(LocalDateTime fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public StatusTopico getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusTopico status) {
+		this.status = status;
+	}
+
+	public Usuario getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Usuario autor) {
+		this.autor = autor;
+	}
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+
+	public List<Respuesta> getRespuestas() {
+		return respuestas;
+	}
+
+	public void setRespuestas(List<Respuesta> respuestas) {
+		this.respuestas = respuestas;
+	}
+	
+	
+	
+	
+
+}
